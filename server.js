8db0ff907334d04963c75c65bb3f215ed769e131
Moamen <moamen.montaser12@gmail.com>
2023-11-02 19:01:39 +0200

    3rd commit

--- server.js
+++ server.js
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
 const app = require('./app');
 const port = 3000; //3sahn a7oto f el listen
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-=======
-const app = require('./app');
-const port = 3000; //3sahn a7oto f el listen
 
-app.listen(port, () => {
-  console.log(`Server running on port: ${port}`);
-});
->>>>>>> 9368eb4 (First commit)
+//hey
